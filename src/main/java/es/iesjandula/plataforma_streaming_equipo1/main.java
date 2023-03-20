55c6ca11c0814cdb10e2d57cc4ac1f047f03a747
DavidMartinezFlores <dmarflo526@g.educaand.es>
2023-03-20 19:52:14 +0100

    Se deja un simple main de inicio.

---
+++ src/main/java/es/iesjandula/plataforma_streaming_equipo1/main.java
@@ -0,0 +1,11 @@
+package es.iesjandula.plataforma_streaming_equipo1;
+
+public class main {
+
+	public static void main(String[] args) {
+		//Main de inicio
+		System.out.println("Hola mundo");
+
+	}
+
+}
