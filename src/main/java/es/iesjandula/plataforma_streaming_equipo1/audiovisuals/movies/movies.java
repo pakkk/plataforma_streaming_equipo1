0d05c9903c4fdd99b4ef6a64554907a65d57463e
pabloSerrano19 <psergut234@g.educaand.es>
2023-03-21 19:13:01 +0100

    tarea008:upload otra vez

--- src/main/java/es/iesjandula/plataforma_streaming_equipo1/audiovisuals/movies/movies.java
+++ src/main/java/es/iesjandula/plataforma_streaming_equipo1/audiovisuals/movies/movies.java
@@ -1,5 +1,52 @@
 package es.iesjandula.plataforma_streaming_equipo1.audiovisuals.movies;
 
-public class movies {
+import java.util.Arrays;
+
+import es.iesjandula.plataforma_streaming_equipo1.actors.Actors;
+import es.iesjandula.plataforma_streaming_equipo1.audiovisuals.Audiovisuals;
+import es.iesjandula.plataforma_streaming_equipo1.audiovisuals.Type;
+import es.iesjandula.plataforma_streaming_equipo1.categories.Categories;
+import es.iesjandula.plataforma_streaming_equipo1.recomendations.Recomendations;
+import es.iesjandula.plataforma_streaming_equipo1.subtitles.Subtitles;
+
+public class movies extends Audiovisuals{
+	protected Recomendations[] recomendationsMovies=new Recomendations[0];
+	
+	
+	/**
+	 * Constructor of the class
+	 * 
+	 * @param movies with the movies info
+	 * 
+	 */
+	
+	public movies(Type type, Actors[] actores, Categories[] categ, String tittle, String originalLenguaje,
+			String[] adaptedLenguajes, Subtitles[] avaliableSubtitles, Double duration, int creationYear) {
+		super(type, actores, categ, tittle, originalLenguaje, adaptedLenguajes, avaliableSubtitles, duration, creationYear);
+		// TODO Auto-generated constructor stub
+	}
+	
+	
+	/* SETTERS AND GETTERS*/
+	public Recomendations[] getRecomendationsMovies() {
+		return recomendationsMovies;
+	}
+
+	public void setRecomendationsMovies(Recomendations recomendationsMovies) {
+		this.recomendationsMovies = Arrays.copyOf(this.recomendationsMovies,this.recomendationsMovies.length+1);
+		this.recomendationsMovies[this.recomendationsMovies.length-1]=recomendationsMovies;
+		
+	}
+	/* SETTERS AND GETTERS*/
+
+
+	@Override
+	public String toString() {
+		return "movies [Movies=" + this.type +" "+Arrays.toString(this.actores)+" "
+				+""+Arrays.toString(this.categ)+" "+this.tittle+" "+this.originalLenguaje +" "+Arrays.toString(this.adaptedLenguajes)+" "
+						+ ""+Arrays.toString(this.avaliableSubtitles)+" "+ this.duration+" "+this.creationYear+"]";
+	}
+	
+	
 
 }
