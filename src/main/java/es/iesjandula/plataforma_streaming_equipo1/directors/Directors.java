b03ea343f9eb18e4877c626cc16dfa99520543bd
1DAM <1DAM@1DAM-09>
2023-03-21 12:48:15 +0100

    Update Directors Comment

--- src/main/java/es/iesjandula/plataforma_streaming_equipo1/directors/Directors.java
+++ src/main/java/es/iesjandula/plataforma_streaming_equipo1/directors/Directors.java
@@ -1,19 +1,21 @@
 package es.iesjandula.plataforma_streaming_equipo1.directors;
 
 public class Directors {
+	//Variables
 	protected String name, lastName, nationality, sex;
     protected int year;
-
+    
+    //Constructor
     public void setDirector(String name, String lastName, int year, String nationality, String sex){
-        this.name=name;
-        this.lastName=lastName;
-        this.year=year;
-        this.nationality=nationality;
-        this.sex=sex;
+        this.name=name; //Name of director
+        this.lastName=lastName; //Last Name of director
+        this.year=year; //Year of birth of director
+        this.nationality=nationality; //Nationality of director
+        this.sex=sex; //Sex of director
     }  
-
+    //Getters and setters
     public String getName() {
-        return name;
+        return name; 
     }
 
     public String getlastName() {
@@ -32,6 +34,7 @@ public class Directors {
         return year;
     }
     
+    //Information of director String 
     public String toString() {
         return "Director: " + " name: " + name + ", lastName: " + lastName + ", nationality: " + nationality + ", sex: " + sex + ", year: " + year;
     }
