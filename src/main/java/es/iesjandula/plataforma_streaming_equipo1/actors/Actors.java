06128e787ac2722b9daaf55638a03e522da0d033
DavidMartinezFlores <dmarflo526@g.educaand.es>
2023-03-23 20:46:15 +0100

    TAREA002: Retoques en directores

--- src/main/java/es/iesjandula/plataforma_streaming_equipo1/actors/Actors.java
+++ src/main/java/es/iesjandula/plataforma_streaming_equipo1/actors/Actors.java
@@ -2,12 +2,16 @@ package es.iesjandula.plataforma_streaming_equipo1.actors;
 
 import es.iesjandula.plataforma_streaming_equipo1.genders.Genders;
 
-public class Actors {
+public class Actors 
+{
 	// Variables
-		protected String name, lastName, nationality;
-		protected Genders gender;
-	    protected int year;
-		public Actors(String name, String lastName, String nationality, Genders gender, int year) {
+		private String name, lastName, nationality;
+		private Genders gender;
+		private int year;
+		
+		//Constructor with the class
+		public Actors(String name, String lastName, String nationality, Genders gender, int year) 
+		{
 			super();
 			this.name = name;
 			this.lastName = lastName;
@@ -16,43 +20,66 @@ public class Actors {
 			this.year = year;
 		}
 		
+		//toString Method, for print the actor.
 		@Override
-		public String toString() {
+		public String toString() 
+		{
 			return "Actors [name=" + name + ", lastName=" + lastName + ", nationality=" + nationality + ", gender=" + gender
-					+ ", year=" + year + "]";
+			+ ", year=" + year + "]";
 		}
-
-		public String getName() {
+		
+		//SETTERS-GETTERS
+		public String getName() 
+		{
+			//Get the name
 			return name;
 		}
-		public void setName(String name) {
+		public void setName(String name) 
+		{
+			//Set the name
 			this.name = name;
 		}
-		public String getLastName() {
+		public String getLastName() 
+		{
+			//Get the last name
 			return lastName;
 		}
-		public void setLastName(String lastName) {
+		public void setLastName(String lastName) 
+		{
+			//Set the last name
 			this.lastName = lastName;
 		}
-		public String getNationality() {
+		public String getNationality() 
+		{
+			//Get the nationality
 			return nationality;
 		}
-		public void setNationality(String nationality) {
+		public void setNationality(String nationality) 
+		{
+			//Set the nationality
 			this.nationality = nationality;
 		}
-		public Genders getGender() {
+		public Genders getGender() 
+		{
+			//Get the gender
 			return gender;
 		}
-		public void setGender(Genders sex) {
-			this.gender = sex;
+		public void setGender(Genders gender) 
+		{
+			//Set the gender
+			this.gender = gender;
 		}
-		public int getYear() {
+		public int getYear() 
+		{
+			//Get the year
 			return year;
 		}
-		public void setYear(int year) {
+		public void setYear(int year) 
+		{
+			//Set the year
 			this.year = year;
 		}
 	    
-	    
+		//SETTERS-GETTERS
 	    
 }
