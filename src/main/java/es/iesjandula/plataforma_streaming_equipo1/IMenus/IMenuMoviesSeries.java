ce180bd3550f39f25291a9231ea9609eec35b64a
Usuario <Usuario@DESKTOP-GVFTA4L>
2023-03-24 14:39:30 +0100

    Creado pero no terminado imenu para movies y series

---
+++ src/main/java/es/iesjandula/plataforma_streaming_equipo1/IMenus/IMenuMoviesSeries.java
@@ -0,0 +1,14 @@
+package es.iesjandula.plataforma_streaming_equipo1.IMenus;
+
+public interface IMenuMoviesSeries {
+	public static void moviesSeriesMenu() 
+	{
+		//Menu text display
+		System.out.println("\n"
+				+"Movies and Series menu\n"
+				+"\n"
+				+"\n"
+				+"\n"
+				+"");
+	}
+}
